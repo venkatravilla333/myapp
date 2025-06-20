9f32f06c6e191db094db6fd9abe6e1033de8a8a1
venkat ravilla <venkatravilla333@gmail.com>
2025-06-20 19:40:37 +0530

    api calls in redux

--- src/State/Global-State/Redux-Concept/React-Components/K.js
+++ src/State/Global-State/Redux-Concept/React-Components/K.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decreaseCount, increaseCount } from '../Redux/Count/actionCreators'
 import { buyCake } from '../Redux/Cakes/actionCreators'
+import { fetchPosts } from '../Redux/ApiCall/actionCreators'
 
 function K() {
 
@@ -11,16 +12,39 @@ function K() {
  var cakes = useSelector((state) => {
       return state.cakeReducer.noOfCakes
  })
+ var apiData = useSelector((state) => {
+      return state.postsReducer
+ })
+  
+  console.log(apiData)
   
   var dispatch = useDispatch()
 
+
+  var getData = () => {
+    dispatch(fetchPosts())
+  }
+
+  console.log('render')
   return (
     <div>
       <h3>K count: {count}</h3> 
       <button onClick={()=>dispatch(increaseCount())}>increase</button>
       <button onClick={()=>dispatch(decreaseCount())}>decrease</button>
       <h3>cakes : {cakes}</h3> 
-      <button onClick={()=>dispatch(buyCake())}>buy cake</button>
+      <button onClick={() => dispatch(buyCake())}>buy cake</button>
+      <br />
+      <br />
+      <button onClick={getData}>get data</button>
+      {
+        apiData.loading ? <h3>Loading</h3> : apiData.error ? <h3>{apiData.error}</h3>
+          : apiData.posts.map((obj) => {
+            return <div key={obj.id} style={{border: '2px solid, red', padding: '10px', margin: '10px'}}>
+              <h3>Title: {obj.title}</h3>
+              <h3>Body: {obj.body}</h3>
+            </div>
+          })
+      }
     </div>
   )
 }
--- src/State/Global-State/Redux-Concept/Redux/store.js
+++ src/State/Global-State/Redux-Concept/Redux/store.js
@@ -1,16 +1,19 @@
 
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { countReducer } from './Count/countReducer'
 import { cakeReducer } from './Cakes/cakeReducer'
+import { postsReducer } from './ApiCall/postsReducer'
+import { thunk } from 'redux-thunk' 
 
 
 var rootReducer = combineReducers({
   countReducer,
   cakeReducer,
+  postsReducer
 })
 
 
-export var store = createStore(rootReducer) 
+export var store = createStore(rootReducer, applyMiddleware(thunk)) 
 console.log(store)
 
 
